c0971761f689c747a9e31b0c92fe7a5d657c0c6d
karanja03 <wambuik03@gmail.com>
2023-11-08 13:17:04 +0300

    Added  routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import LandingPage from './components/LandingPage';
-import Tours from './pages/Tours';
-import Donations from './pages/Donations';
-import Form from './pages/DonationForm';
+// import Tours from './pages/Tours';
+// import Donations from './pages/Donations';
+// import Form from './pages/DonationForm';
 import 'tailwindcss/tailwind.css';
 import LandingPage from './components/LandingPage';
 import WhatDoWeDo from './components/WhatDoWeDo';
 import Accomplishments from './components/WhoAreWe';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path= '/whatwedo' element={<WhatDoWeDo/>}/>
+          <Route path='/aboutus' element={<Accomplishments/>}/>
+          <Route path='/' element ={<LandingPage/>}/>
+        </Routes>
+      </Router>
 
       <Footer />
     </div>
