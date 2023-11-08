76f70d2ba22014bcb5738c456ebc0bd08b2efc2d
karanja03 <wambuik03@gmail.com>
2023-11-08 20:16:02 +0300

    Added changes to the navbar

--- front-end/src/components/HeaderAK.jsx
+++ front-end/src/components/HeaderAK.jsx
@@ -18,7 +18,7 @@ function HeaderAK(){
 
     return(
         <div>
-            <div className=" p-8 mt-12">
+            <div className=" p-8 ">
       <header className="text-center bg-custom  py-8">
         <h1 className="text-4xl font-bold  py-9" style={{backgroundColor:"#027884"}}>Exploring Kibera: A Glimpse into Nairobi's Largest Slum</h1>
       </header>
