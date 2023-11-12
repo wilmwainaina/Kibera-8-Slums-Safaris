01270e93d7cd7b0d3e99250ffdfab15ceb8fe6fc
karanja03 <wambuik03@gmail.com>
2023-11-12 22:35:40 +0300

    Added ages

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import 
- {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
- from 'react-icons/bs'
 
-function Header({OpenSidebar}) {
+
+
+import React from 'react';
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
+
+const headerStyle = {
+  gridArea: 'header',
+  height: '60px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '0 30px 0 30px',
+  boxShadow: '0 6px 7px -3px',
+};
+
+function Header({ OpenSidebar }) {
   return (
-    <header className='header'>
-        <div className='menu-icon'>
-            <BsJustify className='icon' onClick={OpenSidebar}/>
+    <header style={headerStyle}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ marginRight: '10px' }}>
+          <BsJustify style={{ verticalAlign: 'middle', lineHeight: '1px', fontSize: '20px' }} onClick={OpenSidebar} />
         </div>
-        <div className='header-left'>
-            <BsSearch  className='icon'/>
+        <div style={{ marginRight: '10px' }}>
+          <BsSearch style={{ verticalAlign: 'middle', lineHeight: '1px', fontSize: '20px' }} />
         </div>
-        <div className='header-right'>
-            <BsFillBellFill className='icon'/>
-            <BsFillEnvelopeFill className='icon'/>
-            <BsPersonCircle className='icon'/>
-        </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <BsFillBellFill style={{ verticalAlign: 'middle', lineHeight: '1px', fontSize: '20px', marginRight: '5px' }} />
+        <BsFillEnvelopeFill style={{ verticalAlign: 'middle', lineHeight: '1px', fontSize: '20px', marginRight: '5px' }} />
+        <BsPersonCircle style={{ verticalAlign: 'middle', lineHeight: '1px', fontSize: '20px' }} />
+      </div>
     </header>
-  )
+  );
 }
-
 export default Header
--- front-end/src/components/OrgSideBar.jsx
+++ front-end/src/components/OrgSideBar.jsx
@@ -1,49 +1,83 @@
-import React from 'react'
-import 
-{BsFillBarChartFill, BsFillGrid3X3GapFill, BsPeopleFill, 
-  BsListCheck}
- from 'react-icons/bs';
-import { FaSignOutAlt } from 'react-icons/fa';
+
+import React from 'react';
 import { FiUser } from 'react-icons/fi';
+import { BsFillBarChartFill, BsFillGrid3X3GapFill, BsPeopleFill, BsListCheck } from 'react-icons/bs';
+import { FaSignOutAlt } from 'react-icons/fa';
 
-function OrgSidebar({openSidebarToggle}) {
+const OrgSidebar = ({ openSidebarToggle }) => {
+  const sidebarStyle = {
+    display: 'grid',
+    gridArea: 'sidebar',
+    height: '100%',
+    backgroundColor: '#027884',
+    overflowY: 'auto',
+    transition: 'all 0.5s',
+    WebkitTransition: 'all 0.5s',
+    position: 'absolute',
+    zIndex: openSidebarToggle ? 12 : 'initial',
+  };
+
+  const titleStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '15px 30px 0px 30px',
+    marginBottom: '30px',
+  };
+
+  const brandStyle = {
+    marginTop: '15px',
+    fontSize: '20px',
+    fontWeight: '700',
+    color: '#fff',
+  };
+
+  const listItemStyle = {
+    padding: '20px 20px 20px 20px',
+    fontSize: '18px',
+    cursor: 'pointer',
+    color: '#000000',
+    textDecoration: 'none',
+  };
+
   return (
-    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
-        <div className='sidebar-title'>
-            <div className='sidebar-brand'>
-            <FiUser size={32} color="black" /> Admin Page
-            </div>
+    <aside style={sidebarStyle} id="sidebar">
+      <div style={titleStyle} className="sidebar-title">
+        <div style={brandStyle} className="sidebar-brand">
+          <FiUser size={32} color="black" /> Admin Page
         </div>
+      </div>
 
-        <ul className='sidebar-list'>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillBarChartFill size={32} color="black"/> Overview
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillGrid3X3GapFill className='icon'/> Organizations
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsPeopleFill className='icon'/> Donors
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsListCheck className='icon'/> Funds Tracker
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                <FaSignOutAlt size={32} color="black" /> Sign Out
-                </a>
-            </li>
-        </ul>
+      <ul style={{ padding: 0, listStyleType: 'none' }} className="sidebar-list">
+        <li style={listItemStyle} className="sidebar-list-item">
+          <a href="" style={listItemStyle}>
+            <BsFillBarChartFill size={32} color="black" /> Overview
+          </a>
+        </li>
+        <li style={listItemStyle} className="sidebar-list-item">
+          <a href="" style={listItemStyle}>
+            <BsFillGrid3X3GapFill /> Organizations
+          </a>
+        </li>
+        <li style={listItemStyle} className="sidebar-list-item">
+          <a href="" style={listItemStyle}>
+            <BsPeopleFill /> Donors
+          </a>
+        </li>
+        <li style={listItemStyle} className="sidebar-list-item">
+          <a href="" style={listItemStyle}>
+            <BsListCheck /> Funds Tracker
+          </a>
+        </li>
+        <li style={listItemStyle} className="sidebar-list-item">
+          <a href="" style={listItemStyle}>
+            <FaSignOutAlt size={32} color="black" /> Sign Out
+          </a>
+        </li>
+      </ul>
     </aside>
-  )
-}
+  );
+};
 
-export default OrgSidebar
+export default OrgSidebar;
+
