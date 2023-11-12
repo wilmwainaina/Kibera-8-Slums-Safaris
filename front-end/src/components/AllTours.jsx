f5fcfea586d4eae89315b06a9f7c5bd059592f99
karanja03 <wambuik03@gmail.com>
2023-11-12 19:41:48 +0300

    Added new feature in admin dashboard

--- front-end/src/components/AllTours.jsx
+++ front-end/src/components/AllTours.jsx
@@ -1,124 +1,119 @@
 
 
 
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
 
-function AllTours() {
-  const navigate = useNavigate();
 
-  const containerVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+
+
+import React, { useState } from 'react';
+
+const AllTours = () => {
+  // Example state for tonnal donations and booked tours
+  const [donations, setDonations] = useState({
+    totalDonations: 500,
+    totalCompanies: 15,
+    averageDonation: 50,
+    topOrganizations: [
+      { name: 'Organization A', description: 'Description of Organization A' },
+      { name: 'Organization B', description: 'Description of Organization B' },
+      { name: 'Organization C', description: 'Description of Organization C' },
+    ],
+  });
+
+  const [bookedTours, setBookedTours] = useState([
+    { company: 'Company A', date: '2023-12-01', details: 'The Tour will happen on Friday every early in the morning' },
+    { company: 'Company B', date: '2023-12-05', details: 'Details...' },
+    // Add more data as needed
+  ]);
+
+  const [filter, setFilter] = useState(''); // Example filter state, can be date range, organization, etc.
+
+  // Example function for sorting tours by date
+  const sortToursByDate = () => {
+    setBookedTours([...bookedTours].sort((a, b) => new Date(a.date) - new Date(b.date)));
   };
 
-  const handleViewMoreClick = () => {
-    navigate('/aboutkibera');
-  }
-
   return (
-    <div>
-      <div className='ml-72'>
-    <motion.div
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-    >
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-5xl w-full p-8 rounded ml-44 ">
 
-      <div className="mostpopularTitle">
-        <h3 className="text-black text-2xl ml-36 mb-6 mt-6 font-bold">All Tours</h3>
-      </div>
+        {/* Total Donations Card */}
+        <div className="bg-gray-100 rounded shadow-md p-6 mb-4 px-44 py-24">
+          <h2 className="text-2xl font-bold text-teal-600 mb-4">Total Donations</h2>
+          <div className="flex justify-between mb-2">
+            <span className="text-gray-700 font-semibold">Total Donations:</span>
+            <span className="text-teal-600 font-bold">{donations.totalDonations}</span>
+          </div>
+          <div className="flex justify-between mb-2">
+            <span className="text-gray-700 font-bold ">Total Companies:</span>
+            <span className="text-teal-600 font-bold">{donations.totalCompanies}</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-700 font-bold">Average Donation:</span>
+            <span className="text-teal-600 font-bold">{donations.averageDonation}</span>
+          </div>
+          <div>
+            <h3 className="text-lg font-bold text-gray-700 mb-2 text-teal-600 mt-6">Top Organizations</h3>
+            <table className="w-full border-collapse border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="bg-teal-600 text-white py-2 px-4">Organization</th>
+                  <th className="bg-teal-600 text-white py-2 px-4">Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {donations.topOrganizations.map((org, index) => (
+                  <tr key={index}>
+                    <td className="border py-2 px-4">{org.name}</td>
+                    <td className="border py-2 px-4">{org.description}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
 
-      <div className="mostpopularbody ml-36 max-w-screen-xl bg-gray-100 shadow-lg py-24 px-14">
-        <div className="containermostpopular ml-12">
-          <motion.div
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
-            className="cardmostpopular"
-          >
-            <div className="card-header">
-              <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/c1/9a/06.jpg" alt="rover" />
-            </div>
-            <div className="card-bodymostpopular">
-              <h1 className='font-bold  text-xl mb-3'>Victoria Crafts</h1>
-              <p className="text-black">
-            This is a a collective that is at the forefront of collecting trash and repurposing as jewelry. The group is collecting old and fresh bones, brass, and horns, transforming them into jewelry, bowls, and trinkets.          
-                </p>
-              <button
-                type="button"
-                className="btn btn-primary btn-lg learnmore mt-11"
-                style={{ backgroundColor: "#027884" }}
-              >
-                Know More
+        {/* Booked Tours Card */}
+        <div className="bg-gray-100 rounded shadow-md p-6 w-fit mt-14">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-bold text-teal-600">Booked Tours</h2>
+            <div className="flex space-x-4">
+              <button onClick={sortToursByDate} className="bg-teal-600 text-white px-4 py-2 rounded">
+                Sort by Date
               </button>
+              <input
+                type="text"
+                placeholder="Filter by Company"
+                className="border px-2 py-1 rounded"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+              />
             </div>
-          </motion.div>
-
-          <motion.div
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
-            className="cardmostpopular"
-          >
-            <div className="card-header">
-              <img src="https://genetsafaris.com/wp-content/uploads/2016/09/kibera-slum.jpg" alt="balloons" />
-            </div>
-            <div className="card-bodymostpopular">
-            <h1 className='font-bold  text-xl mb-3'> Power Women Group-</h1>
-
-              <p className="text-black">
-             This is a group of 15 women infected and affected by HIV/AIDS. The group was started with the aim of eradicating stigma and discrimination in the society through creation of community awareness on the same disease.              </p>
-              <button
-                type="button"
-                className="btn btn-primary btn-lg learnmore mt-11"
-                style={{ backgroundColor: "#027884" }}
-              >
-                Know More
-              </button>
-            </div>
-          </motion.div>
-
-          <motion.div
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
-            className="cardmostpopular"
-          >
-            <div className="card-header">
-              <img src="https://genetsafaris.com/wp-content/uploads/2016/09/kibera-slum.jpg" alt="balloons" />
-            </div>
-            <div className="card-bodymostpopular">
-            <h1 className='font-bold  text-xl mb-3'>SHOFCO</h1>
-
-              <p className="text-black">
-              This is a grassroots movement that catalyzes large-scale transformation in urban slums by providing critical services for all, community advocacy platforms, and education and leadership development for women and girls.              </p>
-              <button
-                type="button"
-                className="btn btn-primary btn-lg learnmore mt-11"
-                style={{ backgroundColor: "#027884" }}
-              >
-                Know More
-              </button>
-            </div>
-          </motion.div>
+          </div>
+          <table className="w-full border-collapse border border-gray-300">
+            <thead>
+              <tr>
+                <th className="bg-teal-600 text-white py-2 px-4">Company</th>
+                <th className="bg-teal-600 text-white py-2 px-4">Date</th>
+                <th className="bg-teal-600 text-white py-2 px-4">Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookedTours
+                .filter((tour) => tour.company.toLowerCase().includes(filter.toLowerCase()))
+                .map((tour, index) => (
+                  <tr key={index}>
+                    <td className="border py-2 px-4">{tour.company}</td>
+                    <td className="border py-2 px-4">{tour.date}</td>
+                    <td className="border py-2 px-4">{tour.details}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
       </div>
-      <div>
-        <button
-          type="button"
-          className="btn btn-primary btn-lg learnmore mt-11 ml-44 viewmore"
-          style={{ backgroundColor: "#027884", marginLeft: "415px" }}
-          onClick={handleViewMoreClick}
-        >
-          View More
-        </button>
-      </div>
-    </motion.div>
-    </div>
     </div>
   );
-}
+};
 
 export default AllTours;
